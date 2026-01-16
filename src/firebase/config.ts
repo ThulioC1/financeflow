861d2684cfaf6a01aa2120b813dac049d2817d97
Thulio Costa <ThulioCosta8@gmail.com>
2026-01-16 19:02:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6708129872-17770",
+  "appId": "1:282867872165:web:c8e8068a458ac06ec6384c",
+  "apiKey": "AIzaSyDzQMH4VKYjwHLkeKMk2KbPHU2ElboDiWw",
+  "authDomain": "studio-6708129872-17770.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "282867872165"
+};
